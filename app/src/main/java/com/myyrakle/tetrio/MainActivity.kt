c200a230b36f027be38b58c3ed6164cf70b4fbbc
myyrakle <sssang97@naver.com>
2022-06-07 00:20:45 +0900

    1차 완성

--- app/src/main/java/com/myyrakle/tetrio/MainActivity.kt
+++ app/src/main/java/com/myyrakle/tetrio/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.myyrakle.tetrio
 
 import android.app.Instrumentation
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.webkit.WebSettings.LOAD_DEFAULT
-import android.webkit.WebSettings.LOAD_NO_CACHE
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private var webView: WebView? = null
     private var leftButton: Button? = null
     private var rightButton: Button? = null
+    private var xButton: Button? = null
+    private var zButton: Button? = null
+    private var hardButton: Button? = null
+    private var softButton: Button? = null
+    private var holdButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
 
         this.webView = findViewById(R.id.webView)
         webView?.settings?.javaScriptEnabled = true
@@ -41,9 +47,18 @@ class MainActivity : AppCompatActivity() {
         webView?.webViewClient = WebViewClient()
         webView?.loadUrl("https://tetr.io/")
 
+        this.setLeftButton()
+        this.setRightButton()
+        this.setZButton()
+        this.setXButton()
+        this.setHardButton()
+        this.setSoftButton()
+        this.setHoldButton()
+    }
+
+    private fun setLeftButton() {
         this.leftButton = findViewById((R.id.left))
         leftButton?.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "asdf", Toast.LENGTH_LONG)
             Thread(Runnable{
                 Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_LEFT)
             }).start()
@@ -56,11 +71,74 @@ class MainActivity : AppCompatActivity() {
             }).start();
             true
         })
+    }
 
+    private fun setRightButton() {
         this.rightButton = findViewById((R.id.right))
         rightButton?.setOnClickListener(View.OnClickListener {
             Thread(Runnable{
                 Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_RIGHT)
+            }).start()
+        })
+        rightButton?.setOnLongClickListener ( View.OnLongClickListener{
+            Thread(Runnable{
+                for (i in 1..10) {
+                    Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_RIGHT)
+                }
+            }).start();
+            true
+        })
+    }
+
+    private fun setXButton() {
+        this.xButton = findViewById((R.id.x))
+        xButton?.setOnClickListener(View.OnClickListener {
+            Thread(Runnable{
+                Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_UP)
+            }).start()
+        })
+    }
+
+    private fun setZButton() {
+        this.zButton = findViewById((R.id.z))
+        zButton?.setOnClickListener(View.OnClickListener {
+            Thread(Runnable{
+                Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_CTRL_LEFT)
+            }).start()
+        })
+    }
+
+    private fun setHardButton() {
+        this.hardButton = findViewById((R.id.hard))
+        hardButton?.setOnClickListener(View.OnClickListener {
+            Thread(Runnable{
+                Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_SPACE)
+            }).start()
+        })
+    }
+
+    private fun setSoftButton() {
+        this.softButton = findViewById((R.id.soft))
+        softButton?.setOnClickListener(View.OnClickListener {
+            Thread(Runnable{
+                Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN)
+            }).start()
+        })
+        softButton?.setOnLongClickListener ( View.OnLongClickListener{
+            Thread(Runnable{
+                for (i in 1..10) {
+                    Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_DPAD_DOWN)
+                }
+            }).start();
+            true
+        })
+    }
+
+    private fun setHoldButton() {
+        this.holdButton = findViewById((R.id.hold))
+        holdButton?.setOnClickListener(View.OnClickListener {
+            Thread(Runnable{
+                Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_SHIFT_LEFT)
             }).start()
         })
     }
